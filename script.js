f34eb11b00b58d53dbcf11c3fdd71e17a0a1e830
Rob Hurkes <rob.hurkes@dowjones.com>
2015-12-03 14:24:52 -0600

    lots of changes

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var displayType, header, headerWrapper;
+//TODO IIFE
+// TODO prevent scrolling while uimask visible
+var displayType, header, fauxHeader, headerWrapper, menuButton, uimask, sideNav;
+
+header = document.getElementById('header');
+headerWrapper = document.getElementById('header-wrapper');
+fauxHeader = document.getElementById('fauxheader');
+menuButton = document.getElementById('menu-button');
+uimask = document.getElementById('uimask');
+sideNav = document.getElementById('side-nav');
 
 var peekabooConfig = {
   'header': {
@@ -23,15 +32,31 @@ window.addEventListener('resize', function() {
   defineDisplay();
 });
 
+menuButton.addEventListener('click', function() {
+  uimask.classList.toggle('visible');
+  sideNav.classList.toggle('visible');
+});
+
+uimask.addEventListener('click', function() {
+  uimask.classList.toggle('visible');
+  sideNav.classList.toggle('visible');
+});
+
 window.addEventListener('scroll', function() {
   if (window.scrollY > peekabooConfig['header'][displayType]) {
-    header.classList.add('peekaboo');
+    if (!header.classList.contains('peekaboo')) {
+      header.classList.add('peekaboo');
+      fauxHeader.classList.add('peekaboo');
+    }
   } else {
     header.classList.remove('peekaboo');
+    fauxHeader.classList.remove('peekaboo');
   }
 
   if (window.scrollY > peekabooConfig['header-wrapper'][displayType]) {
-    headerWrapper.classList.add('peekaboo');
+    if (!headerWrapper.classList.contains('peekaboo')) {
+      headerWrapper.classList.add('peekaboo');
+    }
   } else {
     headerWrapper.classList.remove('peekaboo');
   }
@@ -47,62 +72,4 @@ function ready(fn) {
 
 ready(function() {
   defineDisplay();
-  header = document.getElementById('header');
-  headerWrapper = document.getElementById('header-wrapper');
 });
-
-// Chart stuff
-google.load('visualization', '1.1', {packages: ['line']});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var fullData = [
-        [91.77, 207.893265, 1452, 880, 44],
-        [92.69, 233.873962, 1509, 880, 45],
-        [93.62, 272.229706, 1767, 857, 47],
-        [94.54, 296.81308, 2043, 925, 45],
-        [95.46, 312.589355, 1920, 955, 47],
-        [96.39, 418.118896, 1944, 1054, 52],
-        [97.31, 466.61264, 1920, 1193, 50],
-        [98.23, 558.502014, 2426, 1244, 48],
-        [99.15, 675.569153, 2425, 1220, 45],
-        [100.08, 688.673462, 1895, 1310, 43],
-        [101, 866.322693, 1351, 1317, 44],
-        [101.92, 1061.252075, 1102, 1397, 42],
-        [102.84, 1291.158813, 1069, 1555, 43],
-        [103.76, 1366.450073, 907, 1591, 35],
-        [104.68, 1502.562256, 865, 1591, 32]
-        ];
-
-        var lclChart = document.getElementById('lcl_chart');
-        var lclValues = [];
-        for (var i = 0; i < fullData.length; i++) {
-          var dr = fullData[i];
-          lclValues.push([dr[0], dr[1], dr[3]]);
-        }
-
-        var lclData = new google.visualization.DataTable();
-        lclData.addColumn('number', 'Longitude (Degrees West)');
-        lclData.addColumn('number', "Elevation");
-        lclData.addColumn('number', "Median LCL");
-        lclData.addRows(lclValues);
-
-        var lclOptions = {
-          chart: { title: 'Median Lifted Condensation Level vs Elevation @ Grid Row 58' },
-          width: 700,
-          height: 500,
-          pointSize: 20,
-          series: {
-            0: {axis: 'Elevation'},
-            1: {axis: 'LCL'}
-          },
-          axes: {
-            y: {
-              Elevation: {label: 'Elevation (m)'},
-              LCL: {label: 'LCL (m)'}
-            }
-          }
-        };
-
-        var chart = new google.charts.Line(lclChart);
-        chart.draw(lclData, lclOptions);
-  }
